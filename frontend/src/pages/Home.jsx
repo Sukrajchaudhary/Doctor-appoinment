e35d9d3f8449a848c0c8383906999808d2fcd55a
Sailesh Rokaya <saileshrokaya123@gmail.com>
2023-12-27 00:09:37 +0545

    Services section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import heroImg01 from "../assets/images/hero-img01.png";
 import heroImg02 from "../assets/images/hero-img02.png";
 import heroImg03 from "../assets/images/hero-img03.png";
@@ -8,6 +8,7 @@ import icon03 from "../assets/images/icon03.png";
 import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
 import About from "../components/About/About";
+import ServiceList from "../components/Services/ServiceList";
 
 const Home = () => {
   return (
@@ -157,7 +158,22 @@ const Home = () => {
           </div>
         </section>
 
-        <About/>
+        <About />
+
+        {/* ================ Services Section Start ================= */}
+        <section>
+          <div className="container">
+            <div className="xl:w-[470px] mx-auto">
+              <h2 className="heading text-center">Our medical services</h2>
+              <p className="text__para text-center">
+                World-class care for everyone. Our health System offers
+                unmatched, expert health care.
+              </p>
+            </div>
+            <ServiceList/>
+          </div>
+        </section>
+        {/* ================ Services Section End ================= */}
       </>
     </>
   );
