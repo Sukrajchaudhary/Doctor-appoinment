1872d10981f476ffe34e12a2b0dda078f3fe1631
Sailesh Rokaya <saileshrokaya123@gmail.com>
2024-01-01 22:40:58 +0545

    User VerifyToken

---
+++ backend/Routes/doctors.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  updateDoctor,
+  deleteDoctor,
+  getAllDoctors,
+  getSingleDoctor,
+} from "../Controllers/doctorController.js";
+import { authenticate, restrict } from "../auth/verifyToken.js";
+
+const router = express.Router();
+
+router.get("/:id", authenticate, restrict(["doctor"]), getSingleDoctor);
+router.get("/", authenticate, restrict(["admin"]), getAllDoctors);
+router.put("/:id", authenticate, restrict(["doctor"]), updateDoctor);
+router.delete("/:id", authenticate, restrict(["doctor"]), deleteDoctor);
+
+export default router;
