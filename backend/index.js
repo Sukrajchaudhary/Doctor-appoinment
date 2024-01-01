e682a2a2f32afc6ebcea9092cefe881cb461a735
Sailesh Rokaya <saileshrokaya123@gmail.com>
2024-01-01 11:21:55 +0545

    user register api

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoute from "./Routes/auth.js";
 
 dotenv.config();
 
@@ -18,24 +19,24 @@ app.get("/", (req, res) => {
 });
 
 // database connection
-mongoose.set('strictQuery', false);
-const connectDB = async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URL,{
-            useNewUrlParser: true,
-            useUnifiedTOpology: true,
-        })
-        console.log('MongoDB database is connected')
-    } catch (err) {
-        console.log('MongoDB database connection failed')
-        
-    }
-}
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTOpology: true,
+    });
+    console.log("MongoDB database is connected");
+  } catch (err) {
+    console.log("MongoDB database connection failed");
+  }
+};
 
 // middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
+app.use('/api/v1/auth', authRoute);
 
 app.listen(port, () => {
   connectDB();
